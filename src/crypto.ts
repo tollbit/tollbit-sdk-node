29cc184a270ab177a0bf57c1ed1baa17d8a7f81c
Josh Mayer <jooshmayer@gmail.com>
2024-03-27 14:33:41 -0700

    cryoto

--- src/crypto.ts
+++ src/crypto.ts
@@ -2,23 +2,23 @@ import { createCipheriv, createDecipheriv, randomBytes } from "crypto";
 
 export function encrypt(plaintext: string, keyString: string): string {
   const key = Buffer.from(keyString, "hex");
-  const iv = randomBytes(16);
+  const iv = randomBytes(12);
   const cipher = createCipheriv("aes-256-gcm", key, iv);
   const encrypted = Buffer.concat([
     cipher.update(plaintext, "utf8"),
     cipher.final(),
   ]);
   const tag = cipher.getAuthTag();
-  return iv.toString("hex") + tag.toString("hex") + encrypted.toString("hex");
+  return iv.toString("hex") + encrypted.toString("hex") + tag.toString("hex");
 }
 
 export function decrypt(encryptedString: string, keyString: string): string {
   const key = Buffer.from(keyString, "hex");
   const encrypted = Buffer.from(encryptedString, "hex");
-  const iv = encrypted.slice(0, 16);
+  const iv = encrypted.slice(0, 12); // Use 12 bytes for the IV
+  const tag = encrypted.slice(-16); // Use the last 16 bytes for the tag
+  const ciphertext = encrypted.slice(12, -16); // The remaining bytes are the ciphertext
   const decipher = createDecipheriv("aes-256-gcm", key, iv);
-  const tag = encrypted.slice(16, 32);
-  const ciphertext = encrypted.slice(32);
   decipher.setAuthTag(tag);
   const decrypted = Buffer.concat([
     decipher.update(ciphertext),
